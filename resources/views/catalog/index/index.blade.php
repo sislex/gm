213e4bd5738e7a71ab84fb6dfcdd19980d16df0b
sislex <sislex@ya.ru>
2016-04-22 20:34:52 +0300

    removed block "search by body"

--- resources/views/catalog/index/index.blade.php
+++ resources/views/catalog/index/index.blade.php
@@ -29,34 +29,34 @@
         <!-- End Main Slider -->
 
         <!-- Utiity Bar -->
-        <div class="utility-bar">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4 col-sm-6 col-xs-8">
-                        <div class="toggle-make">
-                            <a href="#"><i class="fa fa-navicon"></i></a>
-                            <span>Отсортировать по типу кузова</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="by-type-options">
-                <div class="container">
-                    <div class="row">
-                        <ul class="owl-carousel carousel-alt" data-columns="6" data-autoplay="" data-pagination="no" data-arrows="yes" data-single-item="no" data-items-desktop="6" data-items-desktop-small="4" data-items-mobile="3" data-items-tablet="4">
-                            <li class="item"> <a href='{{action('Catalog\CatalogController@index')}}#filter={"Тип кузова":{"0":{"text":"автобус"}}}'><img src="/catalog/images/body-types/wagon.png" alt=""> <span>Wagon</span></a></li>
-                            <li class="item"> <a href="results-list.html"><img src="/catalog/images/body-types/minivan.png" alt=""> <span>Minivan</span></a></li>
-                            <li class="item"> <a href="results-list.html"><img src="/catalog/images/body-types/coupe.png" alt=""> <span>Coupe</span></a></li>
-                            <li class="item"> <a href="results-list.html"><img src="/catalog/images/body-types/convertible.png" alt=""> <span>Convertible</span></a></li>
-                            <li class="item"> <a href="results-list.html"><img src="/catalog/images/body-types/crossover.png" alt=""> <span>Crossover</span></a></li>
-                            <li class="item"> <a href="results-list.html"><img src="/catalog/images/body-types/suv.png" alt=""> <span>SUV</span></a></li>
-                            <li class="item"> <a href="results-list.html#"><img src="/catalog/images/body-types/minicar.png" alt=""> <span>Minicar</span></a></li>
-                            <li class="item"> <a href="results-list.html"><img src="/catalog/images/body-types/sedan.png" alt=""> <span>Sedan</span></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+        {{--<div class="utility-bar">--}}
+            {{--<div class="container">--}}
+                {{--<div class="row">--}}
+                    {{--<div class="col-md-4 col-sm-6 col-xs-8">--}}
+                        {{--<div class="toggle-make">--}}
+                            {{--<a href="#"><i class="fa fa-navicon"></i></a>--}}
+                            {{--<span>Отсортировать по типу кузова</span>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="by-type-options">--}}
+                {{--<div class="container">--}}
+                    {{--<div class="row">--}}
+                        {{--<ul class="owl-carousel carousel-alt" data-columns="6" data-autoplay="" data-pagination="no" data-arrows="yes" data-single-item="no" data-items-desktop="6" data-items-desktop-small="4" data-items-mobile="3" data-items-tablet="4">--}}
+                            {{--<li class="item"> <a href='{{action('Catalog\CatalogController@index')}}#filter={"Тип кузова":{"0":{"text":"автобус"}}}'><img src="/catalog/images/body-types/wagon.png" alt=""> <span>Wagon</span></a></li>--}}
+                            {{--<li class="item"> <a href="results-list.html"><img src="/catalog/images/body-types/minivan.png" alt=""> <span>Minivan</span></a></li>--}}
+                            {{--<li class="item"> <a href="results-list.html"><img src="/catalog/images/body-types/coupe.png" alt=""> <span>Coupe</span></a></li>--}}
+                            {{--<li class="item"> <a href="results-list.html"><img src="/catalog/images/body-types/convertible.png" alt=""> <span>Convertible</span></a></li>--}}
+                            {{--<li class="item"> <a href="results-list.html"><img src="/catalog/images/body-types/crossover.png" alt=""> <span>Crossover</span></a></li>--}}
+                            {{--<li class="item"> <a href="results-list.html"><img src="/catalog/images/body-types/suv.png" alt=""> <span>SUV</span></a></li>--}}
+                            {{--<li class="item"> <a href="results-list.html#"><img src="/catalog/images/body-types/minicar.png" alt=""> <span>Minicar</span></a></li>--}}
+                            {{--<li class="item"> <a href="results-list.html"><img src="/catalog/images/body-types/sedan.png" alt=""> <span>Sedan</span></a></li>--}}
+                        {{--</ul>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
 
         <!-- Start Body Content -->
         <div class="main" role="main">
