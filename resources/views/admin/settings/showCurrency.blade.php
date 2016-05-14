3bc57e3fc5f4a0171ddae4d5cd7c351b8bf051ad
sislex <sislex@ya.ru>
2016-05-14 12:48:48 +0300

    FIX

--- resources/views/admin/settings/showCurrency.blade.php
+++ resources/views/admin/settings/showCurrency.blade.php
@@ -1,75 +1,5 @@
 @extends('admin.layout')
 
-
-
-@section('content')
-            <!-- BEGIN CONTENT BODY -->
-
-    <!-- BEGIN NAV TAB -->
-    <ul class="nav nav-tabs">
-        <li class="active">
-            <a href="#tab_0" data-toggle="tab"> Содержимое </a>
-        </li>
-
-    </ul>
-    <!-- END NAV TAB -->
-
-    <!-- BEGIN FORM-->
-    <form id="currency_form" action="{{action('Admin\SettingsController@updateCurrency')}}" method="post" class="form-horizontal">
-        <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
-        <input type="hidden" name="id" id="id" value="{{ $currency['id'] or '' }}" />
-
-        <div class="tab-content">
-            <div class="tab-pane active" id="tab_0">
-                <div class="portlet box green">
-                    <div class="portlet-title">
-                        <div class="caption">
-                            <i class="fa"></i> Описание валюты
-                        </div>
-                    </div>
-                    <div class="portlet-body form">
-                        <div class="form-body">
-
-                            <div class="form-group">
-                                <label class="col-md-3 control-label"> Наименование (буквенный код) </label>
-                                <div class="col-md-7">
-                                    <input required class="form-control input-circle" name="name" value="{{ $currency['name'] or ''}}" placeholder="Введите наименование валюты">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-md-3 control-label"> Курс по отношению к у.е. </label>
-                                <div class="col-md-7">
-                                    <input required class="form-control input-circle" name="rate" value="{{ $currency['rate'] or ''}}" placeholder="Введите курс к у.е.">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-md-3 control-label"> Знак или краткое наименование </label>
-                                <div class="col-md-7">
-                                    <input required class="form-control input-circle" name="icon" value="{{ $currency['icon'] or ''}}" placeholder="Введите знак валюты">
-                                </div>
-                            </div>
-
-                        </div>
-
-                        <div class="form-actions">
-                            <div class="row">
-                                <div class="col-md-offset-3 col-md-9">
-                                    <button type="submit" class="btn btn-circle green">Сохранить</button>
-                                    <a href="{{action('Admin\SettingsController@showCurrency', ['id' => $currency['id']])}}" class="btn btn-circle red btn-outline">Отменить</a>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </form>
-    <!-- END FORM-->
-
-    <!-- END CONTENT BODY -->
-@endsection
-
 @section('page_bar')
     <div class="page-bar">
         <ul class="page-breadcrumb">
@@ -84,7 +14,28 @@
             </li>
         </ul>
     </div>
+    @endsection
+
+@section('content')
+            <!-- BEGIN CONTENT BODY -->
+
+    <!-- BEGIN NAV TAB -->
+    <ul class="nav nav-tabs">
+        <li class="active">
+            <a href="#tab_0" data-toggle="tab"> Содержимое </a>
+        </li>
+
+    </ul>
+    <!-- END NAV TAB -->
+
+    <!-- BEGIN FORM-->
+
+    <!-- END FORM-->
+
+    <!-- END CONTENT BODY -->
 @endsection
+
+
 
 @section('PAGE-LEVEL-PLUGINS')
     <script src="/admin/assets/global/plugins/fancybox/source/jquery.fancybox.pack.js" type="text/javascript"></script>
