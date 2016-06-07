68af1e1e39164aab0d82209ae096589d248ff35b
vITSaotome <vloseu@gmail.com>
2016-06-08 00:44:45 +0300

    Добавл в админке вывод имени текущего залогиневшегося пользователя.

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -68,24 +68,20 @@ License: You must have a valid license purchased only from themeforest(the above
                 <li class="dropdown dropdown-user">
                     <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
                         <img alt="" class="img-circle" src="/admin/assets/layouts/layout/img/avatar3_small.jpg" />
-                        <span class="username username-hide-on-mobile"> Nick </span>
+                        <span class="username username-hide-on-mobile"> {{ \Auth::user()->name }} </span>
                         <i class="fa fa-angle-down"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-default">
+                        <!--
                         <li>
                             <a href="page_user_profile_1.html">
                                 <i class="icon-user"></i> My Profile </a>
                         </li>
-                        <li>
-                            <a href="app_inbox.html">
-                                <i class="icon-envelope-open"></i> My Inbox
-                                <span class="badge badge-danger"> 3 </span>
-                            </a>
-                        </li>
+                        -->
                         <li class="divider"> </li>
                         <li>
                             <a href="/admin/logout">
-                                <i class="icon-key"></i> Log Out </a>
+                                <i class="icon-key"></i> Logout </a>
                         </li>
                     </ul>
                 </li>
