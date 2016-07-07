f52fb8cd59a73bb2e20521bc788f55847869dd69
sislex <sislex@ya.ru>
2016-07-07 23:08:32 +0300

    mail

--- app/Http/Controllers/Catalog/MailController.php
+++ app/Http/Controllers/Catalog/MailController.php
@@ -11,7 +11,7 @@ class MailController extends Controller
     {
         $input = \Request::all();
 
-        if(isset($input['modal']) || isset($input['callMeBackWidget']) || isset($input['subscribeWidget'])){
+//        if(isset($input['modal']) || isset($input['callMeBackWidget']) || isset($input['subscribeWidget'])){
             $transport = \Swift_SmtpTransport::newInstance('smtp.yandex.ru', 465, 'ssl');
             $transport->setUsername('new.goldenmotors@tut.by');
             $transport->setPassword('Gold3nMotors_2016');
@@ -31,32 +31,29 @@ class MailController extends Controller
                 $mail_data = $input['subscribeWidget'];
             }
 
-            if((isset($mail_data['phone']) && trim($mail_data['phone']) != '') || (isset($mail_data['email']) && trim($mail_data['email']) != '')){
-                if(isset($mail_data['friend']) && trim($mail_data['friend'] != '')){
-                    $mail_sending_result = \Mail::send('catalog.mail.tpl2-share-with-friend', ['mail_data' => $mail_data], function($message) use ($mail_data)
-                    {
-                        $message->from('new.goldenmotors@tut.by', 'Автосалон Golden Motors');
-                        $message->to($mail_data['friend'])->subject("Письмо с сайта [goldenmotors.by]");
-                    });
+            if(isset($mail_data['friend']) && trim($mail_data['friend'] != '')){
+                $mail_sending_result = \Mail::send('catalog.mail.tpl2-share-with-friend', ['mail_data' => $mail_data], function($message) use ($mail_data)
+                {
+                    $message->from('new.goldenmotors@tut.by', 'Автосалон Golden Motors');
+                    $message->to($mail_data['friend'])->subject("Письмо с сайта [goldenmotors.by]");
+                });
 
-                    return 'Письмо отправлено. Код: ' .$mail_sending_result;
+                return 'Письмо отправлено. Код: ' .$mail_sending_result;
 
-                } else {
-                    $mail_sending_result = \Mail::send('catalog.mail.tpl1-general', ['mail_data' => $mail_data], function($message) use ($mail_data)
-                    {
-                        $message->from('new.goldenmotors@tut.by', 'Автосалон Golden Motors');
-                        $message->to('goldenmotors.by@gmail.com', 'Автосалон Golden Motors')->subject("Письмо с сайта [goldenmotors.by] - " .$mail_data['type']);
+            } else {
+                $mail_sending_result = \Mail::send('catalog.mail.tpl1-general', ['mail_data' => $mail_data], function($message) use ($mail_data)
+                {
+                    $message->from('new.goldenmotors@tut.by', 'Автосалон Golden Motors');
+                    $message->to('goldenmotors.by@gmail.com', 'Автосалон Golden Motors')->subject("Письмо с сайта [goldenmotors.by] - " .$mail_data['type']);
 //                        $message->to('sislex@ya.ru', 'Автосалон Golden Motors')->subject("Письмо с сайта [goldenmotors.by] - " .$mail_data['type']);
-                    });
+                });
 
-                    return 'Письмо отправлено. Код: ' .$mail_sending_result;
-                }
-            }else{
-                return 'Письмо не отправлено: не указаны контактные данные.';
+                return 'Письмо отправлено. Код: ' .$mail_sending_result;
             }
-        }else{
-            return 'Ошибка: нет данных для обработки.';
-        }
+
+//        }else{
+//            return 'Ошибка: нет данных для обработки.';
+//        }
 
 //        $files = [
 //            'logo' =>  public_path().'/images/ui-components/logo/'.UIComponents::getLogo(),
